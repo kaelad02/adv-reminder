c322ca0a14dfa77cc96bb7b3064251b46ddb4da1
Chris Seieroe <kaelad02@users.noreply.github.com>
2022-01-21 15:39:28 -0800

    add test cases for AbilitySaveReminder

--- test/reminders.test.js
+++ test/reminders.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "@jest/globals";
-import { AbilityCheckReminder } from "../src/reminders";
+import { AbilityCheckReminder, AbilitySaveReminder } from "../src/reminders";
 
 function createActorWithEffects(...keys) {
   const effects = keys.map(createEffect);
@@ -227,6 +227,217 @@ describe("AbilityCheckReminder both advantage and disadvantage flags", () => {
     const options = {};
 
     const reminder = new AbilityCheckReminder(actor, "str");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBe(true);
+    expect(options.disadvantage).toBeUndefined();
+  });
+});
+
+describe("AbilitySaveReminder no legit active effects", () => {
+  test("saving throw with no active effects should be normal", () => {
+    const actor = createActorWithEffects();
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBeUndefined();
+  });
+
+  test("saving throw with a suppressed active effect should be normal", () => {
+    const actor = createActorWithEffects("flags.midi-qol.advantage.all");
+    actor.effects[0].isSuppressed = true;
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBeUndefined();
+  });
+
+  test("saving throw with a disabled active effect should be normal", () => {
+    const actor = createActorWithEffects("flags.midi-qol.advantage.all");
+    actor.effects[0].data.disabled = true;
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBeUndefined();
+  });
+});
+
+describe("AbilitySaveReminder advantage flags", () => {
+  test("saving throw with advantage.all flag should be advantage", () => {
+    const actor = createActorWithEffects("flags.midi-qol.advantage.all");
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBe(true);
+    expect(options.disadvantage).toBeUndefined();
+  });
+
+  test("saving throw with advantage.ability.all should be advantage", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.advantage.ability.all"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBe(true);
+    expect(options.disadvantage).toBeUndefined();
+  });
+
+  test("saving throw with advantage.ability.save.all should be advantage", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.advantage.ability.save.all"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBe(true);
+    expect(options.disadvantage).toBeUndefined();
+  });
+
+  test("saving throw with advantage.ability.save.wis should be advantage for wis save", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.advantage.ability.save.wis"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBe(true);
+    expect(options.disadvantage).toBeUndefined();
+  });
+
+  test("saving throw with advantage.ability.save.wis should be normal for dex save", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.advantage.ability.save.wis"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "dex");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBeUndefined();
+  });
+});
+
+describe("AbilitySaveReminder disadvantage flags", () => {
+  test("saving throw with disadvantage.all flag should be disadvantage", () => {
+    const actor = createActorWithEffects("flags.midi-qol.disadvantage.all");
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBe(true);
+  });
+
+  test("saving throw with disadvantage.ability.all should be disadvantage", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.disadvantage.ability.all"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBe(true);
+  });
+
+  test("saving throw with disadvantage.ability.save.all should be disadvantage", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.disadvantage.ability.save.all"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBe(true);
+  });
+
+  test("saving throw with disadvantage.ability.save.wis should be disadvantage for wis save", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.disadvantage.ability.save.wis"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "wis");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBe(true);
+  });
+
+  test("saving throw with disadvantage.ability.save.wis should be normal for con save", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.disadvantage.ability.save.wis"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "con");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBeUndefined();
+  });
+});
+
+describe("AbilitySaveReminder both advantage and disadvantage flags", () => {
+  test("saving throw with both advantage and disadvantage should be normal", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.advantage.ability.save.all",
+      "flags.midi-qol.disadvantage.ability.save.dex"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "dex");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBeUndefined();
+  });
+
+  test("saving throw with wrong advantage and same disadvantage should be disadvantage", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.advantage.ability.save.str",
+      "flags.midi-qol.disadvantage.ability.save.cha"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "cha");
+    reminder.updateOptions(options);
+
+    expect(options.advantage).toBeUndefined();
+    expect(options.disadvantage).toBe(true);
+  });
+
+  test("saving throw with same advantage and wrong disadvantage should be advantage", () => {
+    const actor = createActorWithEffects(
+      "flags.midi-qol.advantage.ability.save.str",
+      "flags.midi-qol.disadvantage.ability.save.cha"
+    );
+    const options = {};
+
+    const reminder = new AbilitySaveReminder(actor, "str");
     reminder.updateOptions(options);
 
     expect(options.advantage).toBe(true);
