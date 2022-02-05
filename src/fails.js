a35a9a0b741148e1bbe6a1daa04e33d67da80cfa
Chris Seieroe <kaelad02@users.noreply.github.com>
2022-02-05 13:48:51 -0800

    Change the auto-fail chat message to look like a die roll (#19)
    * add new way to display failed rolls
    
    not removing the original way, just adding this for now
    
    * little refactoring of new code
    
    * cleanup code that uses the failchecker
    
    * remove old fail message code from Fail classes
    
    * remove old fail template
    
    * localize the Auto Fail string
    
    * little bit of formatting
    
    * lots of adjustments to the tests
    
    not done, but getting there
    
    * finish fixing tests
    
    * add FR localization

--- src/fails.js
+++ src/fails.js
@@ -10,55 +10,41 @@ class BaseFail {
     return ["flags.midi-qol.fail.all"];
   }
 
-  get rollString() {
-    "The roll";
-  }
-
-  failCheck() {
-    const effectData = this._getFailEffectData();
-    if (effectData) {
-      this._failRollMessage(effectData);
-      return true;
-    }
-    return false;
-  }
-
-  _getFailEffectData() {
+  /**
+   * Check for auto-fail flags to see if this roll should fail.
+   * @param {object} options the roll options
+   * @returns true if the roll should fail, false if it may continue
+   */
+  async fails(options) {
     // get the active effect keys that will fail
     const failKeys = this.failKeys;
     debug("failKeys", failKeys);
 
-    // search for the fail keys and return the effect data
-    return this.actor.effects
+    const shouldFail = this.actor.effects
       .filter((effect) => !effect.isSuppressed && !effect.data.disabled)
       .flatMap((effect) => effect.data.changes)
-      .filter((change) => failKeys.includes(change.key))
-      .map((change) => change.document?.data)
-      .shift();
+      .some((change) => failKeys.includes(change.key));
+    if (shouldFail) {
+      const messageData = this.createMessageData(options);
+      await this.toMessage(messageData);
+    }
+    return shouldFail;
   }
 
-  async _failRollMessage(effectData) {
-    // generate chat message content
-    const templateData = {
-      effectData,
-      rollString: this.rollString,
-    };
-    const html = await renderTemplate(
-      "modules/adv-reminder/templates/fail-chat-card.html",
-      templateData
+  async toMessage(messageData) {
+    // content that immatates a die roll
+    const content = await renderTemplate(
+      "modules/adv-reminder/templates/fail-dice-roll.hbs"
     );
-
-    const speaker = ChatMessage.getSpeaker({
-      actor: this.actor,
-      token: this.actor.token,
-    });
-    const chatData = {
-      user: game.user.data._id,
-      type: CONST.CHAT_MESSAGE_TYPES.OTHER,
-      content: html,
-      speaker,
-    };
-
+    // merge basic data with child's data
+    const chatData = foundry.utils.mergeObject(
+      {
+        user: game.user.id,
+        type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+        content,
+      },
+      messageData
+    );
     // apply the roll mode to adjust message visibility
     ChatMessage.applyRollMode(chatData, game.settings.get("core", "rollMode"));
 
@@ -84,10 +70,22 @@ export class AbilitySaveFail extends BaseFail {
     ]);
   }
 
-  /** @override */
-  get rollString() {
-    return game.i18n.format("DND5E.SavePromptTitle", {
-      ability: CONFIG.DND5E.abilities[this.abilityId],
+  createMessageData(options = {}) {
+    // build title, probably used as chat message flavor
+    const label = CONFIG.DND5E.abilities[this.abilityId];
+    const title = `${game.i18n.format("DND5E.SavePromptTitle", {
+      ability: label,
+    })}: ${this.actor.name}`;
+
+    // build chat message data
+    const messageData = foundry.utils.mergeObject(options.messageData || {}, {
+      speaker: options.speaker || ChatMessage.getSpeaker({ actor: this.actor }),
+      "flags.dnd5e.roll": { type: "save", abilityId: this.abilityId },
     });
+
+    // pull flavor from a few places before falling back to title
+    messageData.flavor = messageData.flavor || options.flavor || title;
+
+    return messageData;
   }
 }
