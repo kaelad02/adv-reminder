419899d71ab386e4bebb3878eda7fec766e90b2e
Chris Seieroe <kaelad02@users.noreply.github.com>
2023-05-18 18:40:25 -0700

    change RSR to separate message/reminder checks
    instead of using the one isFastForwarding check

--- src/rollers/rsr.js
+++ src/rollers/rsr.js
@@ -31,18 +31,21 @@ export default class ReadySetRollHooks extends CoreRollerHooks {
   preRollAttack(item, config) {
     debug("preRollAttack hook called");
 
-    if (this.isFastForwarding(config)) return;
     const target = getTarget();
 
-    debug("checking for message effects on this attack roll");
-    new AttackMessage(item.actor, target, item).addMessage(config);
-    if (showSources) {
-      debug("checking for adv/dis effects to display their source");
-      new AttackSource(item.actor, target, item).updateOptions(config);
+    if (this._doMessages(config)) {
+      debug("checking for message effects on this attack roll");
+      new AttackMessage(item.actor, target, item).addMessage(config);
+      if (showSources) {
+        debug("checking for adv/dis effects to display their source");
+        new AttackSource(item.actor, target, item).updateOptions(config);
+      }
     }
 
-    debug("checking for adv/dis effects on this attack roll");
-    new AttackReminder(item.actor, target, item).updateOptions(config);
+    if (this._doReminder(config)) {
+      debug("checking for adv/dis effects on this attack roll");
+      new AttackReminder(item.actor, target, item).updateOptions(config);
+    }
   }
 
   preRollAbilitySave(actor, config, abilityId) {
@@ -52,98 +55,121 @@ export default class ReadySetRollHooks extends CoreRollerHooks {
     const failChecker = new AbilitySaveFail(actor, abilityId);
     if (failChecker.fails(config)) return false;
 
-    if (this.isFastForwarding(config)) return;
-
-    debug("checking for message effects on this saving throw");
-    new AbilitySaveMessage(actor, abilityId).addMessage(config);
-    if (showSources) {
-      debug("checking for adv/dis effects to display their source");
-      new AbilitySaveSource(actor, abilityId).updateOptions(config);
+    if (this._doMessages(config)) {
+      debug("checking for message effects on this saving throw");
+      new AbilitySaveMessage(actor, abilityId).addMessage(config);
+      if (showSources) {
+        debug("checking for adv/dis effects to display their source");
+        new AbilitySaveSource(actor, abilityId).updateOptions(config);
+      }
     }
 
-    debug("checking for adv/dis effects on this saving throw");
-    new AbilitySaveReminder(actor, abilityId).updateOptions(config);
+    if (this._doReminder(config)) {
+      debug("checking for adv/dis effects on this saving throw");
+      new AbilitySaveReminder(actor, abilityId).updateOptions(config);
+    }
   }
 
   preRollAbilityTest(actor, config, abilityId) {
     debug("preRollAbilityTest hook called");
 
-    if (this.isFastForwarding(config)) return;
-
-    debug("checking for message effects on this ability check");
-    new AbilityCheckMessage(actor, abilityId).addMessage(config);
-    if (showSources) {
-      debug("checking for adv/dis effects to display their source");
-      new AbilityCheckSource(actor, abilityId).updateOptions(config);
+    if (this._doMessages(config)) {
+      debug("checking for message effects on this ability check");
+      new AbilityCheckMessage(actor, abilityId).addMessage(config);
+      if (showSources) {
+        debug("checking for adv/dis effects to display their source");
+        new AbilityCheckSource(actor, abilityId).updateOptions(config);
+      }
     }
 
-    debug("checking for adv/dis effects on this ability check");
-    new AbilityCheckReminder(actor, abilityId).updateOptions(config);
+    if (this._doReminder(config)) {
+      debug("checking for adv/dis effects on this ability check");
+      new AbilityCheckReminder(actor, abilityId).updateOptions(config);
+    }
   }
 
   preRollSkill(actor, config, skillId) {
     debug("preRollSkill hook called");
 
-    if (this.isFastForwarding(config)) return;
-
-    debug("checking for message effects on this skill check");
-    new SkillMessage(actor, skillId).addMessage(config);
-    if (showSources) {
-      debug("checking for adv/dis effects to display their source");
-      new SkillSource(actor, skillId, true).updateOptions(config);
+    if (this._doMessages(config)) {
+      debug("checking for message effects on this skill check");
+      new SkillMessage(actor, skillId).addMessage(config);
+      if (showSources) {
+        debug("checking for adv/dis effects to display their source");
+        new SkillSource(actor, skillId, true).updateOptions(config);
+      }
     }
 
-    debug("checking for adv/dis effects on this skill check");
-    new SkillReminder(actor, skillId, this.checkArmorStealth).updateOptions(config);
+    if (this._doReminder(config)) {
+      debug("checking for adv/dis effects on this skill check");
+      new SkillReminder(actor, skillId, this.checkArmorStealth).updateOptions(config);
+    }
   }
 
   preRollToolCheck(item, config) {
     debug("preRollToolCheck hook called");
 
-    if (this.isFastForwarding(config)) return;
-
-    debug("checking for message effects on this tool check");
-    new AbilityCheckMessage(item.actor, item.system.ability).addMessage(config);
-    if (showSources) {
-      debug("checking for adv/dis effects to display their source");
-      new AbilityCheckSource(item.actor, item.system.ability).updateOptions(config);
+    if (this._doMessages(config)) {
+      debug("checking for message effects on this tool check");
+      new AbilityCheckMessage(item.actor, item.system.ability).addMessage(config);
+      if (showSources) {
+        debug("checking for adv/dis effects to display their source");
+        new AbilityCheckSource(item.actor, item.system.ability).updateOptions(config);
+      }
     }
 
-    debug("checking for adv/dis effects on this tool check");
-    new AbilityCheckReminder(item.actor, item.system.ability).updateOptions(config);
+    if (this._doReminder(config)) {
+      debug("checking for adv/dis effects on this tool check");
+      new AbilityCheckReminder(item.actor, item.system.ability).updateOptions(config);
+    }
   }
 
   preRollDeathSave(actor, config) {
     debug("preRollDeathSave hook called");
 
-    if (this.isFastForwarding(config)) return;
-
-    debug("checking for message effects on this death save");
-    new DeathSaveMessage(actor).addMessage(config);
-    if (showSources) {
-      debug("checking for adv/dis effects to display their source");
-      new DeathSaveSource(actor).updateOptions(config);
+    if (this._doMessages(config)) {
+      debug("checking for message effects on this death save");
+      new DeathSaveMessage(actor).addMessage(config);
+      if (showSources) {
+        debug("checking for adv/dis effects to display their source");
+        new DeathSaveSource(actor).updateOptions(config);
+      }
     }
 
-    debug("checking for adv/dis effects on this death save");
-    new DeathSaveReminder(actor).updateOptions(config);
+    if (this._doReminder(config)) {
+      debug("checking for adv/dis effects on this death save");
+      new DeathSaveReminder(actor).updateOptions(config);
+    }
   }
 
   preRollDamage(item, config) {
     debug("preRollDamage hook called");
 
-    // check for critical flags unless the user pressed a fast-forward key
-    if (this.isFastForwarding(config)) return;
     const target = getTarget();
 
-    debug("checking for message effects on this damage roll");
-    new DamageMessage(item.actor, target, item).addMessage(config);
-    if (showSources) {
-      debug("checking for adv/dis effects to display their source");
-      new CriticalSource(item.actor, target, item).updateOptions(config);
+    if (this._doMessages(config)) {
+      debug("checking for message effects on this damage roll");
+      new DamageMessage(item.actor, target, item).addMessage(config);
+      if (showSources) {
+        debug("checking for adv/dis effects to display their source");
+        new CriticalSource(item.actor, target, item).updateOptions(config);
+      }
     }
 
-    debug("checking for critical/normal effects on this damage roll");
-    new CriticalReminder(item.actor, target, item).updateOptions(config);
+    if (this._doReminder(config)) {
+      debug("checking for critical/normal effects on this damage roll");
+      new CriticalReminder(item.actor, target, item).updateOptions(config);
+    }
+  }
+
+  _doMessages({ fastForward = false }) {
+    if (fastForward) debug("fast-forwarding the roll, skip messages");
+    return !fastForward;
+  }
+
+  _doReminder({ advantage = false, disadvantage = false }) {
+    if (advantage) debug("advantage already set, skip reminder checks");
+    if (disadvantage) debug("disadvantage already set, skip reminder checks");
+    return !(advantage || disadvantage);
   }
 }
